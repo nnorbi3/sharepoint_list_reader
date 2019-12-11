0cc4d1ebed145c30e2f3f7eb1e348beb6c07868d
Nagy Norbert <n.norbi3@gmail.com>
2019-12-11 11:39:23 +0100

    Data and logic layer created

---
+++ sp_client/Data/Connecter.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Connecter
+    {
+        public string URL { get; }
+
+        public string Username { get; }
+
+        public SecureString Password { get; }
+
+        public ClientContext client;
+
+        public Connecter(string url, string username, string password)
+        {
+            URL = url.Trim();
+            Username = username.Trim();
+            Password = new SecureString();
+            foreach (char c in password)
+            {
+                this.Password.AppendChar(c);
+            }
+        }
+
+        /// <summary>
+        /// Returns an error in case of failure.
+        /// </summary>
+        /// <returns>Error.</returns>
+        public Error ConnectAndAuthenticate()
+        {
+            try
+            {
+                client = new ClientContext(URL);
+
+                try
+                {
+                    try
+                    {
+                        client.Credentials =
+                            new SharePointOnlineCredentials(Username, Password); // Online login
+                        client.ExecuteQuery();
+                    }
+                    catch (NotSupportedException)
+                    {
+                        client.Credentials =
+                            new NetworkCredential(Username, Password); // On-premise(local) login
+                        client.ExecuteQuery();
+                    }
+
+                }
+                catch (ClientRequestException)
+                {
+                    return new Error { Type = ErrorType.AuthenticationError, Message = "Something went wrong. Check your credentials" };
+                }
+                catch (IdcrlException)
+                {
+                    return new Error { Type = ErrorType.AuthenticationError, Message = "Incorrect username or password" };
+                }
+                catch (ArgumentException)
+                {
+                    return new Error { Type = ErrorType.AuthenticationError, Message = "Incorrect username. Username must be an e-mail address" };
+                }
+                catch (WebException)
+                {
+                    return new Error { Type = ErrorType.AuthenticationError, Message = "Something went wrong. Check your credentials" };
+                }
+            }
+            catch (ArgumentException)
+            {
+                return new Error { Type = ErrorType.AuthenticationError, Message = "Requested site not found." };
+            }
+
+
+
+            return new Error { Type = ErrorType.OK };
+        }
+    }
+}
---
+++ sp_client/Logic/SharepointLogic.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class SharepointLogic
+    {
+        private ClientContext client;
+
+        public SharepointLogic(ClientContext client)
+        {
+            this.client = client;
+        }
+
+        public List<string> GetListNames()
+        {
+            List<string> lists = new List<string>();
+            Web site = client.Web;
+            ListCollection coll = site.Lists;
+            IEnumerable<List> resultCollection = client.LoadQuery(
+                coll.Include(
+                    l => l.Title,
+                    l => l.Id,
+                    l => l.DefaultViewUrl,
+                    l => l.BaseType).Where(l => l.BaseType == BaseType.GenericList && !l.Hidden)
+            );
+            client.Load(coll);
+            client.ExecuteQuery();
+            foreach (List l in resultCollection)
+            {
+                if (!l.DefaultViewUrl.Contains("PublishedFeed") &&
+                    l.DefaultViewUrl.Contains("Lists"))
+                {
+                    lists.Add(l.Title);
+                }
+            }
+
+            return lists;
+        }
+    }
+}
